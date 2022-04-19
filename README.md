2bd2240e9b3ca0d951e14e23fd8190ed6430f828
Can Toraman <can.toraman@commsworld.com>
2022-04-19 23:10:05 +0100

    initial commit & add script, readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# List Splitter Script
+
+## Usage
+Provide arguments for the script as such
+
+```python3 list_splitter.py "[0,1,1,2,3,5,8,13,21,34,55]" "3"```
+
+will return
+
+```[[0, 1, 1], [2, 3, 5], [8, 13, 21], [34, 55]] ```
+
+
+Signed: Can Toraman
---
+++ list_splitter.py
@@ -0,0 +1,15 @@
+# list_splitter.py
+
+import sys
+import re
+
+def list_split(list, divider):
+    return [list[i:i+divider] for i in range(0, len(list), divider)]
+
+reg_ex = "([^[]+(?=]))"
+
+list = [int(i) for i in re.search(reg_ex, sys.argv[1]).group().split(",")]
+
+divider = int(sys.argv[2])
+
+print(list_split(list, divider))
